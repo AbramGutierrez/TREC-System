2616200aa53042e25875d6fa86c61878a809a04e
Abram Gutierrez <abram_gutierrez@yahoo.com>
2015-04-03 20:57:22 -0500

    Remigrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403220238) do
+ActiveRecord::Schema.define(version: 20150404014423) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "administrators", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -53,15 +62,6 @@ ActiveRecord::Schema.define(version: 20150403220238) do
     t.string   "school"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
   end
 
 end
