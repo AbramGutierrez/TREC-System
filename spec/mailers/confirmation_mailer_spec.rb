4b1fb10194f05c391d0f87c38b1b051a5452b7dc
Natalie Rawle <natalierawle@tamu.edu>
2015-04-15 12:03:41 -0500

    Finished creating mailer. Needs integration testing or further tests.

--- spec/mailers/confirmation_mailer_spec.rb
+++ spec/mailers/confirmation_mailer_spec.rb
@@ -1,11 +1,14 @@
 require "rails_helper"
 
 RSpec.describe ConfirmationMailer, type: :mailer do
-  it "sends a confirmation email" do
-    @participant = Participant.new()
-    @account = Account.new(first_name: "first", last_name: "last",
-                email: "valid_email@test.com", user: @participant)
+  describe "sends a confirmation email" do
+    let(:participant) { Participant.new() }
+    let(:account) { Account.new(first_name: "first", last_name: "last",
+                email: "valid_email@test.com", user: participant)
+    }
     
-    email = ConfirmationMailer.welcome_email(@account).deliver_now
+    it "and adds to the total mail sent" do
+      expect { ConfirmationMailer.welcome_email(account).deliver_now }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
   end
 end
