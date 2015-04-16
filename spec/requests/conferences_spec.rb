07d6f5dd236163486037389666ad8cb41925674c
Taylor Hunt <thehuntclub@tamu.edu>
2015-04-15 20:12:56 -0500

    Added conference page security

--- spec/requests/conferences_spec.rb
+++ spec/requests/conferences_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Conferences", type: :request do
+  before(:each) do
+		@admin = Administrator.create!()
+		@admin.create_account!(first_name: "TestFirst", last_name: "TestLast",
+			email: "tester@example.com", password: "password",
+			password_confirmation: "password")
+  end
+	
+  after(:each) do
+	@admin.account.delete
+  end
+
   describe "GET /conferences" do
     it "works! (now write some real specs)" do
+	  log_in_as(@admin.account)
       get conferences_path
       expect(response).to have_http_status(200)
     end
