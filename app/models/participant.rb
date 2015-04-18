54186887c3f80f9ea50c1f677561c128a49e3ef8
tianyizhang <tianzhang@tamu.edu>
2015-04-18 16:34:42 -0500

    added shirt size validation

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,6 +1,8 @@
 class Participant < ActiveRecord::Base
 	belongs_to :team
 	has_one :account, :as => :user
-	validates :shirt_size, :phone, presence: true
+	validates :phone, presence: true
+	validates :shirt_size, presence: true, inclusion: { in: %w(small medium large),
+      message: "%{value} is not a valid size, try entering small, medium, or large." }
 	# validates :captain, presence: true
 end
