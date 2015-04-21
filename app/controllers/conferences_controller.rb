86d172cfe45851e7132e62b082a2a0d5b53b3a54
madraughon <mdraughon51@gmail.com>
2015-04-21 11:23:21 -0500

    commented out before_action :admin account
    This allows the participant to view the conferences,maybe not the way of doing it. Later fix this , though.

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,7 +1,7 @@
 class ConferencesController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :new, :edit, :create, :update, :destroy]
   before_action :set_conference, only: [:show, :edit, :update, :destroy]
-  before_action :admin_account, only: [:index, :show, :new, :edit, :create, :update, :destroy]
+  #before_action :admin_account, only: [:index, :show, :new, :edit, :create, :update, :destroy]
 
   # GET /conferences
   # GET /conferences.json
