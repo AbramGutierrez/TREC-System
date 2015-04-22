da558d4688aa370333166300db1ab3455fcd3ffd
tianyizhang <tianzhang@tamu.edu>
2015-04-21 19:16:59 -0500

    added model verification and associations
    team model now check if captain is present upon creation
    
    team now has a has many participants relationship

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,6 @@
 class Team < ActiveRecord::Base
 	belongs_to :conference
+	has_many :participants
 	validates :conference, :school, :paid_status, 
 	  presence: true
 	validates :team_name, uniqueness: true , 
