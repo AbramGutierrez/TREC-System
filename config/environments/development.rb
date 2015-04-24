918be8972bff6a066208acf4b0290f6933c5f7b9
Natalie Rawle <natalierawle@tamu.edu>
2015-04-24 17:48:09 -0500

    Removed error checking for deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,11 +19,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = {
     host: 'localhost',
-    port: 3000
+    port: 25
   }
   config.action_mailer.smtp_settings = {
     :address              => 'smtp.gmail.com',
-    :port                 => 465,
+    :port                 => 587,
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
     :authentication       => 'plain',
