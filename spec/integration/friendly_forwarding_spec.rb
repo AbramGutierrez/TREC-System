9680ab9c15c00e4a057eaaf3ba9e7ca915d3c335
Taylor Hunt <thehuntclub@tamu.edu>
2015-04-27 18:54:52 -0500

    Fixed tests that were broken from new model validations

--- spec/integration/friendly_forwarding_spec.rb
+++ spec/integration/friendly_forwarding_spec.rb
@@ -1,14 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "friendly forwarding", :type => :request do
+	before(:all) do
+		@c = Conference.create!(start_date: Date.parse("2015-4-4"), 
+		  end_date: Date.parse("2015-6-6"),
+		  max_team_size: 6,
+		  min_team_size: 1,
+		  max_teams: 5,
+		  tamu_cost: 30.00,
+		  other_cost: 60.00,
+		  challenge_desc: 'yay!',
+		  is_active: true
+		  )
+	  
+		@team = Team.create!(:conference => @c,	
+		  :school => "TestSchool",
+		  :paid_status => "paid", 
+		  :team_name => "PartControllerTest" 
+		  )
+	end
+
     before(:each) do
 		@p = Participant.create!(captain: false, shirt_size: "large",
-			phone: "1876543211", account_attributes: {first_name: "A", last_name: "Z", email: "p4@example.com",
+			phone: "1876543211", team: @team, account_attributes: {first_name: "A", last_name: "Z", email: "p4@example.com",
 			password: "password", password_confirmation: "password"})
 	end
 	
 	after(:each) do
 		@p.destroy
+	end
+	
+	after(:all) do
+		@team.destroy
+		@c.destroy
 	end
 	
 	it 'user is properly redirected with friendly forwarding' do
