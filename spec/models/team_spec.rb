53009b330263ad2c3c05dbc28829896c5908ee6f
Taylor Hunt <thehuntclub@tamu.edu>
2015-04-28 16:56:57 -0500

    Added max team check

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -74,4 +74,29 @@ RSpec.describe Team, type: :model do
 		)).to_not be_valid
 	end
   
+	it "should not allow the number of teams to exceed the max for a conference" do
+		c1 = Conference.create!(start_date: Date.parse("2015-4-4"), 
+			end_date: Date.parse("2015-6-6"),
+			max_team_size: 6,
+			min_team_size: 1,
+			max_teams: 1,
+			tamu_cost: 30.00,
+			other_cost: 60.00,
+			challenge_desc: 'yay!',
+			)
+			
+		team = Team.create!(:conference => c1,	
+			:school => "TestSchool",
+			:paid_status => "paid", 
+			:team_name => "team1" 
+			)
+			
+		expect(Team.new(
+			:conference => c1,	
+			:school => "TestSchool",
+			:paid_status => "paid",
+			:team_name => "team2" 
+		)).to_not be_valid
+	end
+  
 end
