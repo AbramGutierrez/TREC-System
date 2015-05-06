3c4c13ba0a261911257742025f1f8ef89407082c
tianyizhang <tianzhang@tamu.edu>
2015-05-05 22:05:08 -0500

    add slide show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505203445) do
+ActiveRecord::Schema.define(version: 20150506022202) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "user_id"
@@ -62,6 +62,12 @@ ActiveRecord::Schema.define(version: 20150505203445) do
     t.integer  "order"
     t.text     "question"
     t.text     "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
